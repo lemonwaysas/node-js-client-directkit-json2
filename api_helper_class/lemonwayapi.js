8fe314c7564d86ef9c9aa579e7add07e8621a251
Jonathan Forrider <jd4rider@gmail.com>
2018-10-28 12:06:01 -0600

    new ES6 helper class

---
+++ api_helper_class/lemonwayapi.js
@@ -0,0 +1,29 @@
+/*
+  Title: LemonwayAPI
+  Author: Jonathan Forrider
+  Author Website: http://jfwebdesigns.altervista.org/
+  Description: A Class used to build the connection to the Lemonway Payment Processor
+*/
+import { HTTP } from 'meteor/http'
+
+export default class LemonwayAPI {
+  constructor (params) {
+    this._url = params.url;
+    this._httpMethod = params.httpMethod;  //The URL provided by Lemonway
+    this._walletMethod = params.walletMethod; //Specific Controller methods located on Lemonway's servers
+    this._postData  = params.postData;
+  }
+
+  request (callback) {
+    let options = {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      data: this._postData
+    }
+
+    if (this._url.substring(this._url.length) !== '/') this._url = this._url+'/';
+
+    HTTP.call(this._httpMethod, this._url + this._walletMethod, options, callback);
+  }
+}
